be7ff939acd9be8332470ad721bb0fdeefa2ca91
shitesh <shiteshsaurav@gmail.com>
2016-04-13 01:23:51 -0700

    added file for tfidf score and added words in hindi stopwords

---
+++ headline_generation/python/trial/tfidf_trial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# tfidf with hindi stopwords
+import nltk
+import codecs
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidf = None
+
+def tokenise(text):
+    x = nltk.word_tokenize(text)
+    return_list = []
+    for i in x:
+        i = i.replace(',', '').replace('.', '').replace(u'।', '')
+        if not i:
+            continue
+        return_list.append(i) # can use stemmer here
+    return return_list
+
+def generate_tfidf_values(all_text):
+    global tfidf
+    file = codecs.open('hindi_stopwords.txt', 'r', encoding='utf-8')
+    stop_words = file.readlines()
+    stop_words = [word.strip() for word in stop_words]
+    tfidf = TfidfVectorizer(tokenizer=tokenise, stop_words=stop_words)
+    tfidf.fit_transform(all_text)
+    for name in tfidf.get_feature_names():
+        print name
+
+
+
+def read_file(file_location):
+    file = codecs.open(file_location, 'r', encoding='utf-8')
+    file_contents = []
+    for line in file:
+        file_contents.append(line.strip())
+    return ' '.join(file_contents)
+
+all_text = []
+all_text.append(read_file('hindi_train1.txt'))
+all_text.append(read_file('hindi_train2.txt'))
+
+generate_tfidf_values(all_text)
+
+check = u"रमजान शुरू होते ही इस इलाके में तड़के सुबह लोगों की भीड़ नजर आ जाती है।"
+response = tfidf.transform([check])
+print response
+feature_names = tfidf.get_feature_names()
+for col in response.nonzero()[1]:
+    print feature_names[col], ' - ', response[0, col]
